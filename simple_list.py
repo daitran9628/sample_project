c928f85119bfd788a13c8ddd576a6c2343698631
DaiTra <dvtmail@yahoo.com>
2022-02-17 11:21:20 -0800

    Python File

---
+++ simple_list.py
@@ -0,0 +1,15 @@
+import os
+list1 = [8,3,2,5,7]
+list1.reverse()
+print("the reverse list is ", list1)
+
+list2 = [12,45,34,27,41,32]
+list2.sort()
+print("The sorted list is ", list2)
+
+list3 = []
+list3 = list1.copy()
+print("list3 is ", list3)
+
+indexvalue =  list2[1:4]
+print("the index value are ", indexvalue)
